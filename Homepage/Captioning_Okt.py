54fe0776e004b3f0eccee79ca75a4391a827857b
Jeong Beom <whwjdqja58@naver.com>
2021-11-24 03:11:38 +0900

    211124_JB_서비스테스트(FASTAPI)

--- Homepage/Captioning_Okt.py
+++ Homepage/Captioning_Okt.py
@@ -1,47 +1,45 @@
-# ------------------------ 모델 설명 ------------------------ 
-## 코드는 구글 텐서플로우 튜토리얼 코드를 일부변형하여 작성하였습니다.
-## https://www.tensorflow.org/tutorials/text/image_captioning?hl=ko
 ## MS COCO 데이터를 한글캡셔닝으로 학습시킨 모델 + 멀티모달 데이터 추가활용
 ## 멀티모달 한글 캡셔닝 전처리 : hanspell(맞춤법) 수정
 ### 텐서플로우 케라스 토크나이저를 사용하였으며, 세부내용은 아래와 같습니다.
 
 # 학습 데이터 수 : 147261장(사진), 736582문장(캡션수)
 # 데이터셋 분할 : 훈련 : 검증 : 시험 / 8 : 1 : 1
-# vocab_size = 76344 / max_length = 41
+# vocab_size = 27541 / max_length = 58
 # Batch_size = 64, Buffer_size = 1000, embedding_dim = 256
 # units = 512 / features_shape = 2048 / attention_features_shape = 64
 
-# Epoch 1 : Loss 0.834422 / Loss(val) 1.313634
-# Epoch 2 : Loss 0.489833 / Loss(val) 1.398128
-# Epoch 3 : Loss 0.595815 / Loss(val) 1.683201
-# Epoch 4 : Loss 0.563709 / Loss(val) 1.687328
-# Epoch 5 : Loss 0.537522 / Loss(val) 1.700229
-# Epoch 6 : Loss 0.516654 / Loss(val) 1.706951
-# Epoch 7 : Loss 0.499847 / Loss(val) 1.684718
-# Epoch 8 : Loss 0.485388 / Loss(val) 1.732121
-# Epoch 9 : Loss 0.474416 / Loss(val) 1.731298
-# Epoch 10 : Loss 0.463986 / Loss(val) 1.735539
-# Epoch 11 : Loss 0.457107 / Loss(val) 1.735508
-# Epoch 12 : Loss 0.447212 / Loss(val) 1.737649
-# Epoch 12 : Loss 0.447212 / Loss(val) 1.737649
-# Epoch 14 : Loss 0.433879 / Loss(val) 1.744081
-# Epoch 15 : Loss 0.428988 / Loss(val) 1.747652
-# Epoch 16 : Loss 0.423603 / Loss(val) 1.755263
-# Epoch 17 : Loss 0.419722 / Loss(val) 1.757329
-# Epoch 18 : Loss 0.414604 / Loss(val) 1.762197
-# Epoch 19 : Loss 0.409950 / Loss(val) 1.773284
-# Epoch 20 : Loss 0.407022 / Loss(val) 1.766808
-# Epoch 21 : Loss 0.403701 / Loss(val) 1.771097
-# Epoch 22 : Loss 0.402197 / Loss(val) 1.769002
-# Epoch 23 : Loss 0.399222 / Loss(val) 1.776649
-
-
+# Epoch 1 : Loss 0.543391 / Loss(val) 1.484025
+# Epoch 2 : Loss 0.481019 / Loss(val) 1.494184
+# Epoch 3 : Loss 0.459880 / Loss(val) 1.495114
+# Epoch 4 : Loss 0.445534 / Loss(val) 1.503034
+# Epoch 5 : Loss 0.434286 / Loss(val) 1.526546
+# Epoch 6 : Loss 0.425108 / Loss(val) 1.540671
+# Epoch 7 : Loss 0.417466 / Loss(val) 1.539157
+# Epoch 8 : Loss 0.410838 / Loss(val) 1.556598
+# Epoch 9 : Loss 0.405163 / Loss(val) 1.543960
+# Epoch 10 : Loss 0.400042 / Loss(val) 1.575294
+# Epoch 11 : Loss 0.396082 / Loss(val) 1.556792
+# Epoch 12 : Loss 0.391692 / Loss(val) 1.561010
+# Epoch 13 : Loss 0.388281 / Loss(val) 1.557776
+# Epoch 14 : Loss 0.384969 / Loss(val) 1.557019
+# Epoch 15 : Loss 0.382431 / Loss(val) 1.578591
+# Epoch 16 : Loss 0.379649 / Loss(val) 1.583895
+# Epoch 17 : Loss 0.377440 / Loss(val) 1.582531
+# Epoch 18 : Loss 0.375053 / Loss(val) 1.576649
+# Epoch 19 : Loss 0.372932 / Loss(val) 1.581084
+# Epoch 20 : Loss 0.371102 / Loss(val) 1.583513
+# Epoch 21 : Loss 0.369532 / Loss(val) 1.594376
+# Epoch 22 : Loss 0.367874 / Loss(val) 1.602497
+# Epoch 23 : Loss 0.367510 / Loss(val) 1.584829
+# Epoch 24 : Loss 0.365204 / Loss(val) 1.606700
+# Epoch 25 : Loss 0.364498 / Loss(val) 1.600331
 
 # ------------------------ 모델 구현 파트 ------------------------ 
 
 # 필요 패키지 임포트
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
 import collections
 import random
 import numpy as np
@@ -49,8 +47,7 @@ import os
 import time
 import json
 from PIL import Image
-import pandas as pd
-from hanspell import spell_checker
+from konlpy.tag import Okt
 
 # 모델 학습시 사용한 변수 값들 로드
 # 해당 모델에 들어가야하는 필수 변수들이며, 학습시킬때의 수치와 일치화 시켜주었습니다.
@@ -58,20 +55,17 @@ BATCH_SIZE = 64
 BUFFER_SIZE = 1000
 embedding_dim = 256
 units = 512
-vocab_size = 76344
-max_length = 41
+vocab_size = 27541
+max_length = 58
 features_shape = 2048
 attention_features_shape = 64
-top_k = vocab_size - 1
 
 # 토큰나이저 파일 로드 및 설정
-PATH = 'C:/Workspace/python/빅데이터 지능형서비스 개발 팀프로젝트/Final Project/Data/IC_Data/Model_Data/Captioning/Tensor_Ko_TK_Keras/'
-df_Tk = pd.read_csv(PATH + 'tokenizer(index_word).csv')
-tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
-                                                  oov_token="<unk>",
-                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~')
-tokenizer.word_index = dict(zip(df_Tk['value'], df_Tk['key']))
-tokenizer.index_word = dict(zip(df_Tk['key'], df_Tk['value']))
+PATH = 'C:/Workspace/python/빅데이터 지능형서비스 개발 팀프로젝트/Final Project/Data/IC_Data/Model_Data/Captioning/Tensor_Ko_TK_Okt/'
+df_Tk = pd.read_csv(PATH + 'tokenizer(word_to_index_Okt).csv')
+tokenizer = Okt()
+word_to_index = dict(zip(df_Tk['word'], df_Tk['index']))
+index_to_word = dict(zip(df_Tk['index'], df_Tk['word']))
 
 # 모델구동시 필요한 함수 로드
 def load_image(image_path):
@@ -199,7 +193,8 @@ def evaluate(image):
 
     features = encoder(img_tensor_val)
 
-    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    # dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    dec_input = tf.expand_dims([word_to_index['start']], 0)
     result = []
 
     for i in range(max_length):
@@ -210,16 +205,16 @@ def evaluate(image):
         attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
 
         predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
-        result.append(tokenizer.index_word[predicted_id])
+        result.append(index_to_word[predicted_id])
 
-        if tokenizer.index_word[predicted_id] == '<end>':
+        # if tokenizer.index_word[predicted_id] == '<end>':
+        #     return result, attention_plot
+        if index_to_word[predicted_id] == 'end':
             return result, attention_plot
 
         dec_input = tf.expand_dims([predicted_id], 0)
 
     attention_plot = attention_plot[:len(result), :]
-    result = spell_checker.check(result)
-    result = result.checked
     return result, attention_plot
 
 def plot_attention(image, result, attention_plot):
--- Homepage/Server(FastAPI).py
+++ Homepage/Server(FastAPI).py
@@ -13,9 +13,10 @@ import nest_asyncio
 from PIL.Image import Image
 import shutil
 import json
-import Captioning as cp
+import Captioning_Okt as cp
 import KoGPT2_Branch as kgb
 import KoGPT2_Wise as kgbw
+from hanspell import spell_checker
 
 app = FastAPI()
 
@@ -29,7 +30,7 @@ class Item(BaseModel):
 async def home(request : Request) :
     return templates.TemplateResponse("Service01(input).html", context={"request": request})
 
-@app.post("/service01_test/")
+@app.post("/service01_test01/")
 async def testService(request : Request, img: UploadFile = File(...)):
     path = 'C:/Workspace/python/빅데이터 지능형서비스 개발 팀프로젝트/Final Project/Homepage/static/images/Upload_Images/'
     img_location = path + img.filename
@@ -38,17 +39,37 @@ async def testService(request : Request, img: UploadFile = File(...)):
         file_object.write(img.file.read())
     img = {'image': open(img_location, 'rb')}
 
+    for i in range(1, 6):
+        caption, _ = cp.evaluate(img_location)
+        caption = spell_checker.check(' '.join(caption[:-1]))
+        globals()['caption_' + f'{i}'] = caption.checked
+
+    context = {'request': request, 'img_name' : img_name, 'caption_1' : caption_1, 'caption_2' : caption_2, \
+                'caption_3' : caption_3, 'caption_4' : caption_4, 'caption_5' : caption_5
+                }
+
+    return templates.TemplateResponse("Service01(output01).html", context)
+
+@app.post("/service01_test02/")
+async def testService(request : Request, img_name: str = Form(...), caption: str = Form(...)):
+    path = 'C:/Workspace/python/빅데이터 지능형서비스 개발 팀프로젝트/Final Project/Homepage/static/images/Upload_Images/'
+    img_name = img_name
+    img_location = path + img_name
+
     caption, _ = cp.evaluate(img_location)
     caption = ' '.join(caption[:-1])
 
     sequence_list = []
-    for _ in range(3):
-        sequence = kgbw.result_sequence(caption, 72)
+    for _ in range(2):
+        sequence = kgb.result_sequence(caption, 72)
         sequence_list.append(sequence)
     sequence = ' '.join(sequence_list)
 
-    return templates.TemplateResponse("Service01(output).html", {"request": request, 'img' : img, 'img_name' : img_name,\
-        'caption' : caption, 'sequence' : sequence})
+    context = {
+        "request": request, 'caption' : caption, 'sequence' : sequence, img_name : 'img_name'
+    }
+
+    return templates.TemplateResponse("Service01(output02).html", context)
 
 ngrok_tunnel = ngrok.connect(8000)
 print ('Public URL:', ngrok_tunnel.public_url)
