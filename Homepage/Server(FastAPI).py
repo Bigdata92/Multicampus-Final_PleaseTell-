fef61626dbfce37a8417bf689bcc2443072bd05a
Jeong Beom <whwjdqja58@naver.com>
2021-11-24 17:52:35 +0900

    211124_JB_테스트서비스(FASTAPI)

--- Homepage/Server(FastAPI).py
+++ Homepage/Server(FastAPI).py
@@ -16,6 +16,8 @@ import json
 import Captioning_Okt as cp
 import KoGPT2_Branch as kgb
 import KoGPT2_Wise as kgbw
+import KoGPT2_essay as kgbe
+import yolo5 as y5
 from hanspell import spell_checker
 
 app = FastAPI()
@@ -30,8 +32,8 @@ class Item(BaseModel):
 async def home(request : Request) :
     return templates.TemplateResponse("Service01(input).html", context={"request": request})
 
-@app.post("/service01_test01/")
-async def testService(request : Request, img: UploadFile = File(...)):
+@app.post("/service01_test01/", status_code = 201)
+async def testService01_out01(request : Request, img: UploadFile = File(...), gptModel: str = Form(...)):
     path = 'C:/Workspace/python/빅데이터 지능형서비스 개발 팀프로젝트/Final Project/Homepage/static/images/Upload_Images/'
     img_location = path + img.filename
     img_name = img.filename
@@ -44,29 +46,48 @@ async def testService(request : Request, img: UploadFile = File(...)):
         caption = spell_checker.check(' '.join(caption[:-1]))
         globals()['caption_' + f'{i}'] = caption.checked
 
+    object_list = list(set(y5.yolo(img_location)))
+    object_list[0] = '# ' + object_list[0] 
+    object_list = ' # '.join(object_list)
+
+    gptModel = gptModel
+
     context = {'request': request, 'img_name' : img_name, 'caption_1' : caption_1, 'caption_2' : caption_2, \
-                'caption_3' : caption_3, 'caption_4' : caption_4, 'caption_5' : caption_5
-                }
+                'caption_3' : caption_3, 'caption_4' : caption_4, 'caption_5' : caption_5, 'gptModel' : gptModel, \
+                'object_list' : object_list}
 
     return templates.TemplateResponse("Service01(output01).html", context)
 
-@app.post("/service01_test02/")
-async def testService(request : Request, img_name: str = Form(...), caption: str = Form(...)):
+@app.post("/service01_test02/", status_code = 201)
+async def testService01_out02(request : Request, img_name: str = Form(...), caption: str = Form(...), gptModel: str = Form(...), object_list: str = Form(...)):
     path = 'C:/Workspace/python/빅데이터 지능형서비스 개발 팀프로젝트/Final Project/Homepage/static/images/Upload_Images/'
     img_name = img_name
     img_location = path + img_name
 
-    caption, _ = cp.evaluate(img_location)
-    caption = ' '.join(caption[:-1])
+    caption = caption
+    gptModel = gptModel
+    object_list = object_list
 
     sequence_list = []
-    for _ in range(2):
-        sequence = kgb.result_sequence(caption, 72)
-        sequence_list.append(sequence)
-    sequence = ' '.join(sequence_list)
+    if gptModel == '수필':
+        for _ in range(2):
+            sequence = kgb.result_sequence(caption, 64)
+            sequence_list.append(sequence)
+        sequence = ' '.join(sequence_list)
+    if gptModel == '명언':
+        for _ in range(2):
+            sequence = kgbw.result_sequence(caption, 64)
+            sequence_list.append(sequence)
+        sequence = ' '.join(sequence_list)
+    if gptModel == '일기':
+        for _ in range(2):
+            sequence = kgb.result_sequence(caption, 64)
+            sequence_list.append(sequence)
+        sequence = ' '.join(sequence_list)
 
     context = {
-        "request": request, 'caption' : caption, 'sequence' : sequence, img_name : 'img_name'
+        "request": request, 'caption' : caption, 'sequence' : sequence, 'img_name' : img_name, 'gptModel' : gptModel, \
+        'object_list' : object_list
     }
 
     return templates.TemplateResponse("Service01(output02).html", context)
